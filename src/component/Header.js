e5f1ece711d04303cf4670a40eb49a0f57ffcb94
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-03-29 16:27:03 +0530

    file will be updated

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,25 +1,47 @@
-import Link from "next/link";
 import React, { useState } from "react";
 import { RiArrowDownSLine } from "react-icons/ri";
 import { BsPersonFillAdd } from "react-icons/bs";
 import { FaDonate } from "react-icons/fa";
 import MembershipForm from "../component/MembershipForm";
-import DonateForm from "./DonateForm";
+import DonateForm from "../component/DonateForm";
+import HamburgerMenu from "../component/HamburgerMenu";
+import Link from "next/link";
 
 const Header = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isNavbarOpen, setNavbarOpen] = useState(false);
 
-  const openPopup = () => {
-    setIsPopupOpen(true);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+    setNavbarOpen(!isNavbarOpen);
+
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("open-nav", !isNavbarOpen);
   };
 
-  const closePopup = () => {
-    setIsPopupOpen(false);
+  const [isPopupOpenM, setIsPopupOpenM] = useState(false);
+
+  const openPopupM = () => {
+    setIsPopupOpenM(true);
   };
+
+  const closePopupM = () => {
+    setIsPopupOpenM(false);
+  };
+  const [isPopupOpenD, setIsPopupOpenD] = useState(false);
+
+  const openPopupD = () => {
+    setIsPopupOpenD(true);
+  };
+
+  const closePopupD = () => {
+    setIsPopupOpenD(false);
+  };
+
   return (
     <>
-      <MembershipForm isOpen={isPopupOpen} onClose={closePopup} />
-      <DonateForm isOpen={isPopupOpen} onClose={closePopup} />
+      <MembershipForm isOpen={isPopupOpenM} onClose={closePopupM} />
+      <DonateForm isOpen={isPopupOpenD} onClose={closePopupD} />
       <div className="top-navbar bg5">
         <ul className="v-center wrapper df jce fc1 fw7 pr24 fs14">
           <li className="top-nab-item transit2 p16 fc-h1">
@@ -29,7 +51,7 @@ const Header = () => {
             <Link href="/howyoucanhelp">How You Can Help</Link>
           </li>
           <li className="top-nab-item transit2 p16 fc-h1">
-            <Link href="contact">Contact</Link>
+            <Link href="/contact">Contact</Link>
           </li>
         </ul>
       </div>
@@ -43,17 +65,18 @@ const Header = () => {
               className="logo-img"
             />
           </Link>
-          <div className="menu-btn w48 h48 cp box-center fdc ml32 p8">
+          {/* <div className="menu-btn w48 h48 cp box-center fdc ml32 p8">
             <span className="menu-line"></span>
             <span className="menu-line"></span>
             <span className="menu-line"></span>
-          </div>
+          </div> */}
+          <HamburgerMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
           <div className="navbar v-center fww fs18 fw7">
             <ul className="nav-list v-center cp fww fc3">
               <li className="nav-item transit2 p16 mlr4 fc-h1 ass">
                 <Link href="/about">About IACF</Link>
               </li>
-              <li className="nav-item transit2 p16 mlr4 fc-h1">
+              <li className="nav-item transit2 p16 mlr4 fc-h1 ass">
                 <Link href="/events" className="df pr">
                   Events
                   <RiArrowDownSLine className="material-symbols-outlined ml4 fs22" />
@@ -92,15 +115,15 @@ const Header = () => {
               </li>
             </ul>
             <button
-              onClick={openPopup}
-              className="popup-btn transit2 br4 plr16 h48 bg4 fc1 cp brd3 ml32 v-center"
+              onClick={openPopupM}
+              className="popup-m-btn transit2 br4 plr16 h48 bg4 fc1 cp brd3 ml32 v-center"
             >
               <BsPersonFillAdd className="mr8 fs24" />
               Membership
             </button>
             <button
-              onClick={openPopup}
-              className="popup-btn transit2 br4 plr24 h48 fc1 cp bg1 ml32 v-center"
+              onClick={openPopupD}
+              className="popup-d-btn transit2 br4 plr16 h48 fc1 cp bg1 ml32 v-center"
             >
               <FaDonate className="mr8 fs24" />
               Donate Now
