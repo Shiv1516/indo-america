1462065bfe784d0ebaa5dcc54389ab719eb399dd
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-04-02 17:59:52 +0530

    file will be updated

--- src/component/Events.js
+++ src/component/Events.js
@@ -6,10 +6,10 @@ import Link from "next/link";
 
 const Events = () => {
   return (
-    <section className="events ptb48">
+    <section className="events ptb48 aft pr bg-img-01">
       <div className="events-container wrapper2 df fww aic ptb40">
-        <div className="events-contents flx50 pr32">
-          <h2 className="events-heading fs42 fw7 lh48 mb24 fc6 ff2">
+        <div className="events-contents flx50 pr32 zi2 fc1">
+          <h2 className="events-heading fs42 fw7 lh48 mb24 ff2">
             I WALK - I CARE
           </h2>
           <p className="lh24 mb16">
@@ -39,7 +39,7 @@ const Events = () => {
             </div>
           </Link>
         </div>
-        <div className="events-form flx50 p24 bs3 br4">
+        <div className="events-form flx50 p24 bs3 br4 zi2 bg2">
           <h2 className="form-title fs24 tac fw7 fc6">Event Form</h2>
 
           <div className="flx1 df fdc mb24">
