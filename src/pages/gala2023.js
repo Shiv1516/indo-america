fe4f29bbcccf6530f0b91a90617a0a6aa1ed0fb4
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-04-03 17:40:11 +0530

    file will be updated

--- src/pages/gala2023.js
+++ src/pages/gala2023.js
@@ -116,6 +116,252 @@ const gala2023 = () => {
           </div>
         </div>
       </section>
+
+      <section className="support ptb48">
+        <div className="support-box wrapper2 ptb40">
+          <div className="support-container">
+            <div className="message-heading-box h-center mb40">
+              <div className="message-heading-section tac w75">
+                <h2 className="message-heading fs52 lh48 fc6 fw7 mb24">
+                  Levels of Support for Our Cause{" "}
+                </h2>
+                <p className="message-text lh30 fs18">
+                  Explore Various Ways to Contribute to Our Mission
+                </p>
+              </div>
+            </div>
+            <div className="supports-cards-container df fww aisc ptb24">
+              <div className="support-card flx33 mtb32 df">
+                <div className="support-cards mr32 w100 fc6 fw5 bg12 plr12 br8 brd-b1 ptb32">
+                  <div className="support-cards-content df fww jcsb">
+                    <div>
+                      <h2 className="support-cards-heading mb12 fw7 fs18">
+                        Diamond Supporter :
+                      </h2>
+                      <p className="support-cards-price fs13 fw7 mb24 fc7 it">
+                        $ 25,000
+                      </p>
+                    </div>
+                    <img
+                      src="images/diamond.webp"
+                      alt="diamond"
+                      className="support-cards-img bg1 br50 brd2"
+                    />
+                  </div>
+                  <ul className="support-inner-point">
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Two Premium Tables / 20 Guests.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Naming Rights and Speaking time at the Gala.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Recognition in all Multimedia Presentations.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Full page Ad in Gala Brochure.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="support-card flx33 mtb32 df">
+                <div className="support-cards mr32 w100 fc6 fw5 bg12 plr12 br8 brd-b1 ptb32">
+                  <div className="support-cards-content df fww jcsb">
+                    <div>
+                      <h2 className="support-cards-heading mb12 fw7 fs18">
+                        Sapphire Supporter :
+                      </h2>
+                      <p className="support-cards-price fs13 fw7 mb24 fc7 it">
+                        $ 15,000
+                      </p>
+                    </div>
+                    <img
+                      src="images/sapphire.webp"
+                      alt="sapphire"
+                      className="support-cards-img bg1 br50 brd2"
+                    />
+                  </div>
+                  <ul className="support-inner-point">
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Two Premium Tables / 20 Guests.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Stage time at the Gala.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Recognition in all Multimedia Presentations.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Full page Ad in Gala Brochure.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="support-card flx33 mtb32 df">
+                <div className="support-cards mr32 w100 fc6 fw5 bg12 plr12 br8 brd-b1 ptb32">
+                  <div className="support-cards-content df fww jcsb">
+                    <div>
+                      <h2 className="support-cards-heading mb12 fw7 fs18">
+                        Ruby Supporter :
+                      </h2>
+                      <p className="support-cards-price fs13 fw7 mb24 fc7 it">
+                        $ 10,000
+                      </p>
+                    </div>
+                    <img
+                      src="images/ruby.webp"
+                      alt="ruby-webp"
+                      className="support-cards-img bg1 br50 brd2"
+                    />
+                  </div>
+                  <ul className="support-inner-point">
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      One Premium Table / 10 guests.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Recognition in all Multimedia Presentations.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Full page Ad in Gala Brochure.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="support-card flx33 mtb32 df">
+                <div className="support-cards mr32 w100 fc6 fw5 bg12 plr12 br8 brd-b1 ptb32">
+                  <div className="support-cards-content df fww jcsb">
+                    <div>
+                      <h2 className="support-cards-heading mb12 fw7 fs18">
+                        Emerald Supporter :
+                      </h2>
+                      <p className="support-cards-price fs13 fw7 mb24 fc7 it">
+                        $ 5,000
+                      </p>
+                    </div>
+                    <img
+                      src="images/emerald.webp"
+                      alt="emerald.webp"
+                      className="support-cards-img bg1 br50 brd2"
+                    />
+                  </div>
+                  <ul className="support-inner-point">
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      One Premium Table / 10 guests.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Recognition in all Multimedia Presentations.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Half page Ad in Gala Brochure.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="support-card flx33 mtb32 df">
+                <div className="support-cards mr32 w100 fc6 fw5 bg12 plr12 br8 brd-b1 ptb32">
+                  <div className="support-cards-content df fww jcsb">
+                    <div>
+                      <h2 className="support-cards-heading mb12 fw7 fs18">
+                        Amethyst Supporter :
+                      </h2>
+                      <p className="support-cards-price fs13 fw7 mb24 fc7 it">
+                        $ 2,500
+                      </p>
+                    </div>
+                    <img
+                      src="images/amethyst.webp"
+                      alt="amethyst"
+                      className="support-cards-img bg1 br50 brd2"
+                    />
+                  </div>
+                  <ul className="support-inner-point">
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      One Table / 10 guests.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Quarter page Ad in Gala Brochure.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="support-card flx33 mtb32 df">
+                <div className="support-cards mr32 w100 fc6 fw5 bg12 plr12 br8 brd-b1 ptb32">
+                  <div className="support-cards-content df fww jcsb">
+                    <div>
+                      <h2 className="support-cards-heading mb12 fw7 fs18">
+                        Citrine Supporter :
+                      </h2>
+                      <p className="support-cards-price fs13 fw7 mb24 fc7 it">
+                        $ 1,000
+                      </p>
+                    </div>
+                    <img
+                      src="images/citrine.jpg"
+                      alt="citrine"
+                      className="support-cards-img bg1 br50 brd2"
+                    />
+                  </div>
+                  <ul className="support-inner-point">
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Two Premium Tickets.
+                    </li>
+                    <li className="inner-point mb12 df lh24">
+                      <VscArrowCircleRight className="mr8 fs24 fc5 fw5" />
+                      Name Mention in Gala Brochure.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="events ptb48 bg7">
+        <div className="message-heading-box h-center ptb40">
+          <div className="message-heading-section tac fc1 w75">
+            <h2 className="message-heading fs52 lh48 fw7 mb24">
+              Levels of Support for Our Cause{" "}
+            </h2>
+            <p className="message-text lh30 fs18">
+              Explore Various Ways to Contribute to Our Mission
+            </p>
+          </div>
+        </div>
+        <div className="event-container wrapper2 pt24">
+          <div className="events-img df flx100 p12">
+            <img
+              src="images/gallery/event-img-01.png"
+              alt="event-img"
+              className="w100 br4"
+            />
+          </div>
+          <div className="events-img df flx100 p12">
+            <img
+              src="images/gallery/event-img-02.png"
+              alt="event-img"
+              className="w100 br4"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
