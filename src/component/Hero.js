5e5d6b662129b041ce1a384e30cd65e3b0828318
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-04-18 17:30:53 +0530

    file will be updated

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
+import { fetchBannerData } from "../pages/api/bannerApi";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Link from "next/link";
